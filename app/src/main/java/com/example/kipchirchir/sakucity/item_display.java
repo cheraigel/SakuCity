f5aa5992b99b06ea37d4cca9bf05a7a5e0206d5d
kipchirchir <cheraigel.kipchirchir@gmail.com>
2018-12-09 20:28:14 +0300

    Continued commit

---
+++ app/src/main/java/com/example/kipchirchir/sakucity/item_display.java
@@ -0,0 +1,13 @@
+package com.example.kipchirchir.sakucity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class item_display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_display);
+    }
+}
